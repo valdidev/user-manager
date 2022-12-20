4b0ca34477e4c89fd8de44cdc9c81ff1fcfd082b
valdidev <iamvaldidev@gmail.com>
2022-12-20 19:38:37 +0100

    feat: guest users private route

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const DefaultLayout = (props) => {
+
+    const { user, token } = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />
+    }
+
     return (
         <div>
             default
