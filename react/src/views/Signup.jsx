3a8a377a1f8a0e0df1c7c8c16a125ab97dde2c36
valdidev <iamvaldidev@gmail.com>
2022-12-20 20:28:50 +0100

    feat: signup component done

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,3 +1,25 @@
+import { Link } from "react-router-dom";
+
 export default function Signup() {
-    return <div>Signup</div>;
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Signup for free</h1>
+                    <input type="text" placeholder="Nickname" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password confirmation" />
+                    <button className="btn btn-block">Signup</button>
+                </form>
+                <p className="message">
+                    Already registered? <Link to="/login">Sign in</Link>
+                </p>
+            </div>
+        </div>
+    );
 }
