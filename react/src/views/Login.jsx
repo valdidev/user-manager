1b6866f07492137ccc5f6a098148bd7da183c3e6
valdidev <iamvaldidev@gmail.com>
2022-12-20 20:19:44 +0100

    feat: default user logged

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,3 +1,25 @@
+import { Link } from 'react-router-dom';
+
 export default function Login() {
-    return <div>Login</div>;
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className='title'>
+                        Login into your account
+                    </h1>
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button className="btn btn-block">Login</button>
+                </form>
+                <p className="message">
+                    Not registered? <Link to="/signup">Create an account</Link>
+                </p>
+            </div>
+        </div>
+    );
 }
