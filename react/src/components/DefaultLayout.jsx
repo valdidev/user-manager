682355104c1404fb3dff5cb881cc837045efa202
valdidev <iamvaldidev@gmail.com>
2022-12-20 19:12:38 +0100

    feat: basic structure of components created

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,8 @@
+const DefaultLayout = props => {
+    return (
+      <div>DefaultLayout</div>
+    )
+  }
+  
+  
+  export default DefaultLayout
---
+++ react/src/components/GuestLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+
+const GuestLayout = props => {
+  return (
+    <div>
+        <Outlet  />
+    </div>
+  )
+}
+
+
+export default GuestLayout
