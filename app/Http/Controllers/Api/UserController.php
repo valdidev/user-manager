7a90283a52151b7c39a0f1a52d7df1e37dfb1a05
valdidev <iamvaldidev@gmail.com>
2022-12-24 16:35:57 +0100

    feat: user controller: crud update

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -57,7 +57,15 @@ class UserController extends Controller
      */
     public function update(UpdateUserRequest $request, User $user)
     {
-        //
+        $data = $request->validated();
+
+        if (isset($data['password'])) {
+            $data['password'] = bcrypt($data['password']);
+        }
+
+        $user->update($data);
+
+        return new UserResource($user);
     }
 
     /**
