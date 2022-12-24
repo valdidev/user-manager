ee42f9c69b86f1f6c6c20f13bcbd2891d3e97d89
valdidev <iamvaldidev@gmail.com>
2022-12-24 11:30:33 +0100

    feat: ref hook

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,8 +1,20 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 export default function Signup() {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+
     const onSubmit = (e) => {
         e.preventDefault();
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        };
     };
 
     return (
@@ -10,10 +22,18 @@ export default function Signup() {
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Signup for free</h1>
-                    <input type="text" placeholder="Nickname" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Password confirmation" />
+                    <input ref={nameRef} type="text" placeholder="Nickname" />
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        ref={passwordConfirmationRef}
+                        type="password"
+                        placeholder="Password confirmation"
+                    />
                     <button className="btn btn-block">Signup</button>
                 </form>
                 <p className="message">
