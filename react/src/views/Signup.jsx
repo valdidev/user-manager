d80d1766ca9ff4280af41f59d8e026d63dc39078
valdidev <iamvaldidev@gmail.com>
2022-12-24 11:41:45 +0100

    feat: front signup prepare

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,5 +1,7 @@
 import { useRef } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Signup() {
     const nameRef = useRef();
@@ -7,14 +9,31 @@ export default function Signup() {
     const passwordRef = useRef();
     const passwordConfirmationRef = useRef();
 
+    const { setUser, setToken } = useStateContext();
+
     const onSubmit = (e) => {
         e.preventDefault();
+
         const payload = {
             name: nameRef.current.value,
             email: emailRef.current.value,
             password: passwordRef.current.value,
             password_confirmation: passwordConfirmationRef.current.value,
         };
+
+        axiosClient
+            .post("/signup", payload)
+            .then(({ data }) => {
+                setUser(data.user);
+                setToken(data.token);
+            })
+            .catch((err) => {
+                const response = err.response;
+
+                if (response && response.status === 422) {
+                    console.log(response.data.errors);
+                }
+            });
     };
 
     return (
