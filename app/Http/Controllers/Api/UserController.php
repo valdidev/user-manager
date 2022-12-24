56808fdb97bc4bf1673149341e463815c80ee753
valdidev <iamvaldidev@gmail.com>
2022-12-24 16:31:36 +0100

    feat: user controller crud: store and show

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,17 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resorces\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        return UserResource::collection(
+            User::query()->orderBy('id', 'desc')->paginate(10)
+        );
     }
 
     /**
@@ -27,7 +30,11 @@ class UserController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-        //
+        $data = $request->validated();
+        $data['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+
+        return response(new UserResource($user), 201);
     }
 
     /**
@@ -38,7 +45,7 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        //
+        return new UserResource($user);
     }
 
     /**
