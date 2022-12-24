1c8b1af3fd31aeef869df1064c2b8ae8d1623b35
valdidev <iamvaldidev@gmail.com>
2022-12-24 16:06:37 +0100

    wip: users component waiting for server

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from "react";
+import axiosClient from "../axios-client";
+
 export default function Users() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getUsers()
+    }, []);
+
+    const getUsers = () => {
+        setLoading(true);
+
+        axiosClient
+            .get("/users")
+            .then(({ data }) => {
+                setLoading(false);
+                console.log(data);
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+    };
+
     return <div>Users</div>;
 }
