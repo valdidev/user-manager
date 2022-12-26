b734e37e29a1e9b00902f32c4610cc5513b551a9
valdidev <iamvaldidev@gmail.com>
2022-12-26 18:11:59 +0100

    wip: user form errors

--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -5,6 +5,7 @@ import axiosClient from "../axios-client";
 export default function UserForm() {
     const { id } = useParams();
 
+    const [errors, setErrors] = useState(null);
     const [loading, setLoading] = useState(false);
 
     const [user, setUser] = useState({
@@ -30,5 +31,20 @@ export default function UserForm() {
         }, []);
     }
 
-    return <div>user form</div>;
+    return (
+        <>
+            {user.id && <h1>Update User: {user.name}</h1>}
+            {!user.id && <h1>New User</h1>}
+            <div className="card animated fadeInDown">
+                {loading && <div className="text-center">Loading ...</div>}
+                {errors && (
+                    <div className="alert">
+                        {Object.keys(errors).map((key) => (
+                            <p key={key}>{errors[key][0]}</p>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </>
+    );
 }
