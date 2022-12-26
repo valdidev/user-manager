c5dc905590ad02debca05251656fcbb98d394a47
valdidev <iamvaldidev@gmail.com>
2022-12-26 18:04:52 +0100

    wip: user edit

--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -1,12 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axiosClient from "../axios-client";
 
 export default function UserForm() {
-    const {} = useParams();
+    const { id } = useParams();
 
-    const [user, setUser] = useState();
+    const [loading, setLoading] = useState(false);
+
+    const [user, setUser] = useState({
+        id: null,
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+    });
 
     if (id) {
-
+        useEffect(() => {
+            setLoading(true);
+            axiosClient
+                .get(`/users/${id}`)
+                .then(({ data }) => {
+                    setLoading(false);
+                    setUser(data);
+                })
+                .catch(() => {
+                    setLoading(false);
+                });
+        }, []);
     }
 
     return <div>user form</div>;
